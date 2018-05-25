1f6724fa65b66f2973d56c222ac0e4b70783c872
cauim <cauimsouza@gmail.com>
2018-05-25 15:02:55 +0200

    Make relation class serializable with boost

--- include/relation.h
+++ include/relation.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <stdexcept>
+#include <boost/serialization/vector.hpp>
 
 template<typename T>
 class Relation {
@@ -18,6 +19,12 @@ private:
 	tuples_t tuples; 
 
 	/* member private functions */
+	friend class boost::serialization::access;
+	template<class Archive>
+	void serialize(Archive &ar, const unsigned int version) {
+		ar & tuples;
+		ar & arity;
+	}
 public:
 	/* public typenames */
 	using iterator = typename tuples_t::iterator;
@@ -26,18 +33,21 @@ public:
 	explicit Relation(std::size_t n) : arity{n} {
 		if (n <= 0) throw std::invalid_argument("arity should be\
 				positive integer"); }
-	Relation(const Relation& rel) : arity{rel.arity}, tuples{rel.tuples} {}
-	Relation(Relation&& rel) : arity{rel.arity}, tuples{rel.tuples} {}
 
-	Relation& operator=(Relation&& rel) {
-		arity = rel.arity;
-		tuples = rel.tuples;
-		return *this;
-	}
+	/* copy constructor */
+	Relation(const Relation &rel) : arity{rel.arity}, tuples{rel.tuples} {}
 
+	/* move constructor */
+	Relation(Relation &&rel) : arity{rel.arity}, tuples{std::move(rel.tuples)} {rel.arity = 0; rel.tuples.clear();}
+
+	/* copy assignment */
+	Relation& operator=(const Relation &rel);
+
+	/* move assignment */
+	Relation& operator=(Relation &&rel);
 	/* destructors */
 
-	/* member public funcitons */
+	/* member public functions */
 	std::size_t size() { return tuples.size(); }
 	std::size_t get_arity() { return arity; }
 	iterator begin() { return tuples.begin(); }
@@ -45,5 +55,32 @@ public:
 
 	void push_tuple(tuple_t tpl) { tuples.push_back(tpl); }
 };
+
+/* copy assignment */
+template<typename T>
+Relation<T>& Relation<T>::operator=(const Relation &rel) {
+	if (this == &rel)
+		return *this;
+
+	arity = rel.arity;
+	tuples = rel.tuples;
+
+	return *this;
+}
+
+/* move assignment */
+template<typename T>
+Relation<T>& Relation<T>::operator=(Relation &&rel) {
+	if (this == &rel)
+		return *this;
+
+	arity = rel.arity;
+	tuples = std::move(rel.tuples);
+
+	rel.arity = 0;
+	rel.tuples.clear();
+
+	return *this;
+}
 
 #endif
